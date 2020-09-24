d0c9f4dd19b1162f94e6bd3c77755e94499f9439
Nikita <faster20092009@yandex.ru>
2020-09-24 18:17:36 +0300

    Домашне задание 1

--- Tests/WPFTests/MainWindow.xaml.cs
+++ Tests/WPFTests/MainWindow.xaml.cs
@@ -16,36 +16,8 @@ namespace WPFTests
         }
         public void OnSendButtonClick(object sender, RoutedEventArgs e)
         {
-           
 
-
-            var from = Sender.From;
-            var to = Sender.To;
-
-            var message = Sender.Message;
-           
-            var client = new SmtpClient(Sender.Host, Sender.Port);
-            client.EnableSsl = true;
-
-            client.Credentials = new NetworkCredential
-            {
-                UserName = LoginEdit.Text,
-                SecurePassword = PasswordEdit.SecurePassword
-            };            
-            try
-            {
-                client.Send(message);
-            }
-            catch (Exception ex)
-            {
-                ErrorWindow error = new ErrorWindow();
-                error.txtError.Text = $"Описание ошибки: {ex.Message}";
-                error.ShowDialog();
-            }
-
-            SendEndWindow sew = new SendEndWindow();
-            sew.ShowDialog();
-
+            EmailSendServiceClass.sendSpam(LoginEdit.Text, PasswordEdit.SecurePassword, subject.Text, mailText.Text);
 
         }
     }
