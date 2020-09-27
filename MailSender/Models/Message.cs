7fd5536a38756d8e35dc12e7ae592b17b5668e69
Nikita <faster20092009@yandex.ru>
2020-09-27 13:05:06 +0300

    Создание моделей

---
+++ MailSender/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Models
+{
+    class Message
+    {
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ MailSender/Models/Server.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MailSender.Models
+{
+    class Server
+    {
+        public string Adress { get; set; }
+
+        private int _Port;
+        public int Port 
+        { 
+            get => _Port;
+            set 
+            {
+                if (value < 0 || value >= 65535)
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "Номер порта должен находиться в диапазоне от 0 до 65535");
+                _Port = value;
+            } 
+        }
+
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+        public bool UseSSL { get; set; }
+    }
+}
