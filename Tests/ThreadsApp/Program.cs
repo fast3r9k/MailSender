62029ee194f03294822fa3e679a7dd459a631899
Nikita <faster20092009@yandex.ru>
2020-10-09 02:51:44 +0300

    Домашнее задание к уроку 5

---
+++ Tests/ThreadsApp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsApp
+{
+    class Program
+    {
+        static int[] MyArray;
+        static ulong[] result;
+        static ulong finally_result1 = 1;
+        static ulong finally_result2 = 1;
+
+        static void Thread1()
+        {
+            ulong temp = 1;
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                temp *= Convert.ToUInt64(MyArray[i]);
+            }
+            result[0] = Convert.ToUInt64((temp == 0) ? 1 : temp);
+            finally_result1 *= result[0]; 
+            Console.WriteLine($"Первый поток работу закончил! Результат вычисления - {finally_result1}");
+        }
+        static void Thread2()
+        {
+            ulong temp = 0;
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                temp += Convert.ToUInt64(MyArray[i]);
+            }
+            result[1] = Convert.ToUInt64((temp == 0) ? 1 : temp);
+            finally_result2 = result[1];
+            Console.WriteLine($"Второй поток работу закончил! Результат сложения - {finally_result2}");
+        }
+        static void Main(string[] args)
+        {
+            result = new ulong[2];
+
+            var time = DateTime.Now;
+            Console.WriteLine("Введите число, факториал и сумму которого нужно посчитать");
+            int num = Convert.ToInt32(Console.ReadLine());
+            MyArray = new int[num];
+            //Заполняем массив, длина которого = числу с клавиатуры
+            for (int i = 1; i <= num; i++) { MyArray[i - 1] = i; }
+            //параметры потоков
+            Thread thrd_1 = new Thread(new ThreadStart(Thread1));
+            Thread thrd_2 = new Thread(new ThreadStart(Thread2));
+            thrd_1.Start();
+            thrd_2.Start();
+            //Ждём вычисления                         
+            while (thrd_1.IsAlive || thrd_2.IsAlive) { Thread.Sleep(1000); }
+            Console.WriteLine("Основной поток работу закончил!");
+            Console.WriteLine($"Время выполнения {DateTime.Now - time}");
+            
+            Console.ReadLine();
+        }
+               
+    }
+}
---
+++ Tests/ThreadsApp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Text;
+using System.Text.Unicode;
+using System.Threading;
+
+namespace ThreadsApp2
+{
+    class Program
+    {
+        static ConcurrentQueue<string[]> fields = new ConcurrentQueue<string[]>();
+        static void Main(string[] args)
+        {
+            Thread ThreadParse = new Thread(new ThreadStart(CsvFileParser));
+            Thread ThreadWrite = new Thread(new ThreadStart(CsvToTxt));
+
+            ThreadParse.Start();
+            while (ThreadParse.IsAlive) { Thread.Sleep(100); }
+            ThreadWrite.Start(); 
+
+            Console.WriteLine("Основной поток работу закончил\n");
+            Console.ReadKey();
+        }
+
+        static void CsvFileParser()
+        {
+            Console.WriteLine("Поток 1 стартовал\n");
+            var utf8 = Encoding.UTF8;
+            string path = @"Test.csv";
+            using (TextFieldParser parser = new TextFieldParser(path, utf8))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(";", ",");
+                while (!parser.EndOfData)
+                {
+                    fields.Enqueue(parser.ReadFields());
+                }
+            }
+        }
+
+        static void CsvToTxt()
+        {
+            Console.WriteLine("Поток 2 стартовал\n");
+            string path = @"Test.txt";
+            using (StreamWriter SW = new StreamWriter(path))
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                foreach (string[] lines in fields)
+                {
+                    foreach (string field in lines)
+                    {
+                        SW.Write($"{field}\t ");
+                    }
+                    SW.WriteLine();
+                }
+                Console.WriteLine("Второй поток работу закончил, проверьте файл Test.txt!\n");
+            }
+        }
+    }
+}
