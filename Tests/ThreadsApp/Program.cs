8b0b2c5edd900251c644c6541285835847597f1d
Nikita <faster20092009@yandex.ru>
2020-10-12 21:47:26 +0300

    Задача с матрицами

--- Tests/ThreadsApp/Program.cs
+++ Tests/ThreadsApp/Program.cs
@@ -1,59 +1,48 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ThreadsApp
 {
     class Program
     {
-        static int[] MyArray;
-        static ulong[] result;
-        static ulong finally_result1 = 1;
-        static ulong finally_result2 = 1;
+        static Random rand = new Random();
 
-        static void Thread1()
-        {
-            ulong temp = 1;
-            for (int i = 0; i < MyArray.Length; i++)
-            {
-                temp *= Convert.ToUInt64(MyArray[i]);
-            }
-            result[0] = Convert.ToUInt64((temp == 0) ? 1 : temp);
-            finally_result1 *= result[0]; 
-            Console.WriteLine($"Первый поток работу закончил! Результат вычисления - {finally_result1}");
-        }
-        static void Thread2()
-        {
-            ulong temp = 0;
-            for (int i = 0; i < MyArray.Length; i++)
-            {
-                temp += Convert.ToUInt64(MyArray[i]);
-            }
-            result[1] = Convert.ToUInt64((temp == 0) ? 1 : temp);
-            finally_result2 = result[1];
-            Console.WriteLine($"Второй поток работу закончил! Результат сложения - {finally_result2}");
-        }
+        static int[,] matrix_1, matrix_2, matrix_3;
+        static DateTime time;
+
+        static int N = 100; //размерность матриц
+
         static void Main(string[] args)
         {
-            result = new ulong[2];
+            matrix_1 = new int[N, N];
+            matrix_2 = new int[N, N];
+            matrix_3 = new int[N, N];
 
-            var time = DateTime.Now;
-            Console.WriteLine("Введите число, факториал и сумму которого нужно посчитать");
-            int num = Convert.ToInt32(Console.ReadLine());
-            MyArray = new int[num];
-            //Заполняем массив, длина которого = числу с клавиатуры
-            for (int i = 1; i <= num; i++) { MyArray[i - 1] = i; }
-            //параметры потоков
-            Thread thrd_1 = new Thread(new ThreadStart(Thread1));
-            Thread thrd_2 = new Thread(new ThreadStart(Thread2));
-            thrd_1.Start();
-            thrd_2.Start();
-            //Ждём вычисления                         
-            while (thrd_1.IsAlive || thrd_2.IsAlive) { Thread.Sleep(1000); }
-            Console.WriteLine("Основной поток работу закончил!");
-            Console.WriteLine($"Время выполнения {DateTime.Now - time}");
-            
-            Console.ReadLine();
+            time = DateTime.Now;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+
+                    matrix_1[i, j] = rand.Next(0, 9);
+                    matrix_2[i, j] = rand.Next(0, 9);
+
+                }
+            }
+
+            Parallel.For(0, N, (i, state) =>
+            {
+                Parallel.For(0, N, (j, state2) =>
+                {
+                   matrix_3[i, j] = matrix_1[i, j] * matrix_2[i, j];
+                });
+            });
+
+           Console.WriteLine("Посчитано за {0} секунд", (DateTime.Now - time).TotalSeconds);
+            Console.ReadKey();
+
         }
-               
     }
 }
